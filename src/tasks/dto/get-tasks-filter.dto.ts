5547b73723a55438731f5c981a5bdcfb6d51cc58
Carlos Oliveira <carlosoliveiradev@gmail.com>
2019-09-14 19:19:15 -0300

    Add more some validations

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
 import { TaskStatus } from '../tasks.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn(TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE)
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
