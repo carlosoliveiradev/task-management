ab67d4fe20f83e14b70d0cf264a66ecf91b61e62
Carlos Oliveira <carlosoliveiradev@gmail.com>
2019-10-07 22:21:25 -0300

    add update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,10 +7,13 @@ import {
   ValidationPipe,
   UsePipes,
   ParseIntPipe,
+  Patch,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { TasksStatusValidationPipe } from './pipes/tasks-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -28,6 +31,14 @@ export class TasksController {
   }
 
   deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.tasksService.deleteTask(id) ;
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TasksStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
